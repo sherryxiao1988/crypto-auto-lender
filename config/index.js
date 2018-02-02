6b45ca815e03592d98664cb39b65043787cbcefc
Shundan Xiao <xiaoshundan@gmail.com>
2018-02-02 10:56:45 +0800

    Add strategy for btc

--- config/index.js
+++ config/index.js
@@ -46,6 +46,19 @@ module.exports = {
 			lowerByPercent: 5
 		    }
 		},
+		btc: {
+		    active: true,
+		    minimumRate: 4,
+		    minimumSizeUSD: 50,
+		    rateCreationStrategy: {
+			name: 'topOfTheBook',
+		    },
+		    rateUpdateStrategy: {
+			name: 'lowerRateWithTime',
+			lowerAfterMinutes: 30,
+			lowerByPercent: 5
+		    }
+		},
 		xrp: {
 		    active: true,
 		    minimumRate: 4,
